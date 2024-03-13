ef36058c8a010737cc3ffdf91d079d6130872d23
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-03-13 11:47:00 +0100

    added route to verify user

---
+++ src/app/api/auth/verify.ts
@@ -0,0 +1,15 @@
+import prisma from "@/lib/prisma";
+
+export async function GET(req: Request, params: { id: string }) {
+  const { id } = params;
+  const result = await prisma.user.update({
+    where: {
+      id: id,
+    },
+    data: {
+      verified: true,
+    },
+  });
+  if (!result) return Response.json({ message: "error", status: 500 });
+  return Response.json({ message: "ok", status: 200, data: result });
+}

97af8a3a725182e9c0cacada60dbb7c7619f2546
yannick <yannick.schlaepfer@lernende.bbw.ch>
2024-03-14 15:15:48 +0100

    Added searchbar to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-'use client'
-import React from 'react'
+import { useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaFilter } from "react-icons/fa";
 
-export default function Home() {
+const page = () => {
   return (
-   <div>
-
-   </div>
+    <div className="flex justify-center items-center flex-row">
+      <form className="mt-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-4">
+        <div className=" pointer-events-none">
+          <FaMagnifyingGlass />
+        </div>
+        <input
+          type="text"
+          className=" w-full rounded-md text-center bg-transparent text-lg"
+          placeholder="Suche"
+        />
+        <div className=" pointer-events bg-black rounded-r-lg py-4 px-4">
+          <FaFilter />
+        </div>
+      </form>
+    </div>
   );
-}
+};
+
+export default page;

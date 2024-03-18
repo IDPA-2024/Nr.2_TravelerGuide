1994376f755515fe527e932fa05ada5ad7b1b261
yannick <yannick.schlaepfer@lernende.bbw.ch>
2024-03-18 12:47:10 +0100

    added transparent placeholder text on focus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ import { FaFilter } from "react-icons/fa";
 const page = () => {
   return (
     <div className="flex justify-center items-center flex-row">
-      <form className="mt-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-4">
-        <div className=" pointer-events-none">
+      <form className="mt-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-3">
+        <div>
           <FaMagnifyingGlass />
         </div>
         <input
           type="text"
-          className=" w-full rounded-md text-center bg-transparent text-lg"
+          className=" w-full rounded-md text-center bg-transparent text-lg pl-3 border-none focus:outline-none focus:placeholder:text-transparent"
           placeholder="Suche"
         />
-        <div className=" pointer-events bg-black rounded-r-lg py-4 px-4">
+        <div className=" bg-black rounded-r-lg py-4 px-5">
           <FaFilter />
         </div>
       </form>

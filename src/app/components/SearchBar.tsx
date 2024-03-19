13fb886f7102fe80a9cc5187d8c62ae027bb8377
yannick <yannick.schlaepfer@lernende.bbw.ch>
2024-03-19 11:24:38 +0100

    fixed part of button component and made searchbar a component

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,24 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaFilter } from "react-icons/fa";
+
+const SearchBar = () => {
+  return (
+    <div className="flex justify-center items-center flex-row ">
+      <form className=" absolute top-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-3">
+        <div>
+          <FaMagnifyingGlass />
+        </div>
+        <input
+          type="text"
+          className=" w-full rounded-md text-center bg-transparent text-lg pl-3 border-none focus:outline-none focus:placeholder:text-transparent"
+          placeholder="Suche"
+        />
+            <div className=" bg-black rounded-r-lg py-4 px-5 cursor-pointer">
+              <FaFilter />
+            </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
-import { useState } from "react";
-import { FaMagnifyingGlass } from "react-icons/fa6";
-import { FaFilter } from "react-icons/fa";
+import SearchBar from "./components/SearchBar";
+
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center flex-row">
-      <form className="mt-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-3">
-        <div>
-          <FaMagnifyingGlass />
-        </div>
-        <input
-          type="text"
-          className=" w-full rounded-md text-center bg-transparent text-lg pl-3 border-none focus:outline-none focus:placeholder:text-transparent"
-          placeholder="Suche"
-        />
-        <div className=" bg-black rounded-r-lg py-4 px-5">
-          <FaFilter />
-        </div>
-      </form>
+    <div>
+      <SearchBar/>
     </div>
   );
 };
 
 export default page;
+

6a0db0dac45b4919f684a7f0f083bccb40ba1853
yannick <yannick.schlaepfer@lernende.bbw.ch>
2024-03-20 15:16:07 +0100

    start with checkboxes from filter

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,10 +1,31 @@
+"use client";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { FaFilter } from "react-icons/fa";
+import * as React from "react";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+import CheckboxFormgroup from "./CheckboxLabels";
+
 
 const SearchBar = () => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <div className="flex justify-center items-center flex-row ">
-      <form className=" absolute top-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-3">
+      <div className=" absolute top-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-3">
         <div>
           <FaMagnifyingGlass />
         </div>
@@ -13,10 +34,27 @@ const SearchBar = () => {
           className=" w-full rounded-md text-center bg-transparent text-lg pl-3 border-none focus:outline-none focus:placeholder:text-transparent"
           placeholder="Suche"
         />
-            <div className=" bg-black rounded-r-lg py-4 px-5 cursor-pointer">
-              <FaFilter />
-            </div>
-      </form>
+        <button
+          aria-describedby={id}
+          onClick={handleClick}
+        >
+          <div className=" bg-black rounded-r-lg py-4 px-5">
+            <FaFilter />
+          </div>
+        </button>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+        >
+          <Typography><CheckboxFormgroup /></Typography>
+        </Popover>
+      </div>
     </div>
   );
 };

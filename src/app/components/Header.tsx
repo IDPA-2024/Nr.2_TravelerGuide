e9c3aa7b6aac8d7600dfcb77470542e524b46419
Evan <evan.lueber@stud.kbw.ch>
2024-03-27 14:17:34 +0100

    pointed out filter when open

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,8 +5,13 @@ import * as React from "react";
 import CustomButton from "./CustomButton";
 import Checkbox from "./CheckboxLabels";
 
-const Header = () => {
-  const [filterOpen, setFilterOpen] = React.useState(false);
+const Header = ({
+  filterOpen,
+  setFilterOpen,
+}: {
+  filterOpen: boolean;
+  setFilterOpen: (value: boolean) => void;
+}) => {
   const [userMenuOpen, setUserMenuOpen] = React.useState(false);
   const [filterOptions, setFilterOptions] = React.useState([
     { label: "Asiatisch", value: "asian", checked: false },
@@ -37,10 +42,9 @@ const Header = () => {
       if (checkedValues.includes(option.value)) {
         return { ...option, checked: true };
       } else {
-        return { ...option};
+        return { ...option };
       }
-    }
-    );
+    });
     setFilterOptions(newFilterOptions);
     // TODO: API call with checkedValues
 
@@ -48,53 +52,60 @@ const Header = () => {
   };
 
   return (
-    <div className="flex justify-center items-center flex-row ">
-      <div className=" absolute top-7 flex flex-row items-center justify-center placeholder-gray-300 bg-gray-700 md:w-1/4 rounded-lg pl-3 ">
+    <div className="flex justify-center items-center flex-row">
+      <div className=" z-20 absolute top-7 flex flex-row items-center justify-center bg-black/50 backdrop-filter backdrop-blur-md md:w-1/4 rounded-lg pl-3 ">
         <div>
           <FaMagnifyingGlass />
         </div>
         <input
           type="text"
-          className=" w-full rounded-md text-center bg-transparent text-lg pl-3 border-none focus:outline-none focus:placeholder:text-transparent"
+          className=" w-full rounded-md text-center bg-transparent text-lg pl-3 placeholder:text-white/50 border-none focus:outline-none focus:placeholder:text-transparent"
           placeholder="Suche"
         />
         <button onClick={handleClick}>
-          <div className=" bg-black rounded-r-lg py-4 px-5">
+          <div className=" md:hover:bg-black transition duration-150 ease-in-out cursor-pointer rounded-r-lg py-4 px-5 ">
             <FaFilter />
           </div>
         </button>
         {filterOpen && (
-          <form
-            className="flex flex-col justify-center w-full bg-black/50 rounded-xl shadow-lg shadow-black backdrop-filter py-5 backdrop-blur-md md:h-[55vh] absolute top-full right-0 mt-5 p-5 md:w-1/2"
-            onSubmit={(e) => {
-              handleFilter(e);
-            }}
-          >
-            <div>
-              {filterOptions.map((option) => (
-                <Checkbox
-                  label={option.label}
-                  checked={option.checked}
-                  onChange={() => {
-                    let newFilterOptions = filterOptions.map((filterOption) => {
-                      if (filterOption.label === option.label) {
-                        return { ...filterOption, checked: !filterOption.checked };
-                      } else {
-                        return { ...filterOption };
-                      }
-                    });
-                    setFilterOptions(newFilterOptions);
-                  }}
-                />
-              ))}
-            </div>
-            <CustomButton
-              text="Schliessen"
-              size="custom"
-              custom=" h-8 w-full "
-              type="submit"
-            />
-          </form>
+          <div className="flex flex-col justify-center bg-[#78797A] rounded-xl shadow-lg shadow-black py-5 absolute top-full right-0 mt-5 p-5 md:w-1/2 w-full ">
+            <form
+              onSubmit={(e) => {
+                handleFilter(e);
+              }}
+              className="flex flex-col gap-2"
+            >
+              <div className="md:flex md:flex-col md:justify-center grid grid-cols-2">
+                {filterOptions.map((option) => (
+                  <Checkbox
+                    label={option.label}
+                    checked={option.checked}
+                    onChange={() => {
+                      let newFilterOptions = filterOptions.map(
+                        (filterOption) => {
+                          if (filterOption.label === option.label) {
+                            return {
+                              ...filterOption,
+                              checked: !filterOption.checked,
+                            };
+                          } else {
+                            return { ...filterOption };
+                          }
+                        }
+                      );
+                      setFilterOptions(newFilterOptions);
+                    }}
+                  />
+                ))}
+              </div>
+              <CustomButton
+                text="Schliessen"
+                size="custom"
+                custom=" h-8 w-full "
+                type="submit"
+              />
+            </form>
+          </div>
         )}
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+'use client'
 import Header from "./components/Header";
 import Map from "./components/Map";
 import OverviewDrawer from "./components/OverviewDrawer";
+import { useState } from "react";
 
 const page = () => {
+  const [filterOpen, setFilterOpen] = useState(false);
+
   return (
     <div>
-      {/* <OverviewDrawer/> */}
+        {/* <OverviewDrawer/> */}
       <Map />
-      <Header/>
+      <div className={"absolute bg-black/75 top-0 left-0 right-0 bottom-0 z-10  " + (filterOpen ? "block" : "hidden")}/>
+      <Header filterOpen={filterOpen} setFilterOpen={setFilterOpen} />
     </div>
   );
 };

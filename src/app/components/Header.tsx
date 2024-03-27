c535540e6d9b1fba0d27b93cab559bd333f85c17
Evan <evan.lueber@stud.kbw.ch>
2024-03-27 16:08:17 +0100

    popover of profil done

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,15 +4,19 @@ import { FaFilter } from "react-icons/fa";
 import * as React from "react";
 import CustomButton from "./CustomButton";
 import Checkbox from "./CheckboxLabels";
+import Link from "next/link";
 
 const Header = ({
   filterOpen,
   setFilterOpen,
+  userMenuOpen,
+  setUserMenuOpen
 }: {
   filterOpen: boolean;
   setFilterOpen: (value: boolean) => void;
+  userMenuOpen: boolean;
+  setUserMenuOpen: (value: boolean) => void;
 }) => {
-  const [userMenuOpen, setUserMenuOpen] = React.useState(false);
   const [filterOptions, setFilterOptions] = React.useState([
     { label: "Asiatisch", value: "asian", checked: false },
     { label: "Griechisch", value: "greek", checked: false },
@@ -23,10 +27,17 @@ const Header = ({
     { label: "Sandwhich", value: "sandwhich", checked: false },
     { label: "Sonstiges", value: "other", checked: false },
   ]);
+  let profile = {
+    __html: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 256 256" version="1.1"><rect fill="#e0e0e0" cx="128" width="256" height="256" cy="128" r="128"/><text x="50%" y="50%" style="color: #000000; line-height: 1;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;" alignment-baseline="middle" text-anchor="middle" font-size="112" font-weight="400" dy=".1em" dominant-baseline="middle" fill="#000000">EL</text></svg>`,
+  };
 
-  const handleClick = () => {
+  const handleOpenFilter = () => {
     setFilterOpen(!filterOpen);
     // TODO: API call with checkedValues
+  };
+
+  const handleOpenMenu = () => {
+    setUserMenuOpen(!userMenuOpen);
   };
 
   const handleFilter = (e: React.FormEvent<HTMLFormElement>) => {
@@ -52,8 +63,8 @@ const Header = ({
   };
 
   return (
-    <div className="flex justify-center items-center flex-row">
-      <div className=" z-20 absolute top-7 flex flex-row items-center justify-center bg-black/50 backdrop-filter backdrop-blur-md md:w-1/4 rounded-lg pl-3 ">
+    <div className="flex justify-center items-center flex-row ">
+      <div className=" z-20 absolute top-7 flex flex-row items-center justify-center bg-black/50 backdrop-filter backdrop-blur-md md:w-1/4 w-2/3 mr-24 md:mr-0 rounded-lg pl-3 ">
         <div>
           <FaMagnifyingGlass />
         </div>
@@ -62,13 +73,13 @@ const Header = ({
           className=" w-full rounded-md text-center bg-transparent text-lg pl-3 placeholder:text-white/50 border-none focus:outline-none focus:placeholder:text-transparent"
           placeholder="Suche"
         />
-        <button onClick={handleClick}>
+        <button onClick={handleOpenFilter}>
           <div className=" md:hover:bg-black transition duration-150 ease-in-out cursor-pointer rounded-r-lg py-4 px-5 ">
             <FaFilter />
           </div>
         </button>
         {filterOpen && (
-          <div className="flex flex-col justify-center bg-[#78797A] rounded-xl shadow-lg shadow-black py-5 absolute top-full right-0 mt-5 p-5 md:w-1/2 w-full ">
+          <div className="flex flex-col justify-center bg-[#78797A] rounded-xl shadow-lg shadow-black absolute top-full right-0 mt-5 p-5 md:w-1/2 w-full ">
             <form
               onSubmit={(e) => {
                 handleFilter(e);
@@ -105,6 +116,21 @@ const Header = ({
                 type="submit"
               />
             </form>
+          </div>
+        )}
+      </div>
+      <div className="absolute right-4 top-7 md:right-7 z-20">
+        <div
+          className="rounded-full w-12 h-12 border border-dotted border-black  overflow-hidden cursor-pointer  "
+          onClick={handleOpenMenu}
+        >
+          <div dangerouslySetInnerHTML={profile} className=" w-full h-full" />
+        </div>
+        {userMenuOpen && (
+          <div className="flex flex-col gap-2 justify-center bg-[#78797A] rounded-xl shadow-lg shadow-black mt-2 p-5 w-56 absolute right-0 top-full ">
+            <div className="cursor-pointer">Mein Konto</div>
+            <Link href="/restaurant">Restaurant hinzufügen</Link>
+            <div className="border-t border-white mt-2 pt-2 cursor-pointer">Abmelden</div>
           </div>
         )}
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Header from "./components/Header";
 import Map from "./components/Map";
 import OverviewDrawer from "./components/OverviewDrawer";
@@ -6,13 +6,24 @@ import { useState } from "react";
 
 const page = () => {
   const [filterOpen, setFilterOpen] = useState(false);
+  const [userMenuOpen, setUserMenuOpen] = useState(false);
 
   return (
     <div>
-        {/* <OverviewDrawer/> */}
+      {/* <OverviewDrawer/> */}
       <Map />
-      <div className={"absolute bg-black/75 top-0 left-0 right-0 bottom-0 z-10  " + (filterOpen ? "block" : "hidden")}/>
-      <Header filterOpen={filterOpen} setFilterOpen={setFilterOpen} />
+      <div
+        className={
+          "absolute bg-black/75 top-0 left-0 right-0 bottom-0 z-10  " +
+          (filterOpen || userMenuOpen ? "block" : "hidden")
+        }
+      />
+      <Header
+        filterOpen={filterOpen}
+        setFilterOpen={setFilterOpen}
+        userMenuOpen={userMenuOpen}
+        setUserMenuOpen={setUserMenuOpen}
+      />
     </div>
   );
 };

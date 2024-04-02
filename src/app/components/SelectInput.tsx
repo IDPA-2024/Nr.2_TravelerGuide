afb759eb68fba83eed495b7f1ef91e2c3752512f
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-04-02 11:30:48 +0200

    added Styled Input

---
+++ src/app/components/SelectInput.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+
+const SelectInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(3),
+    color: "white",
+  },
+  "& .MuiSvgIcon-root": {
+    color: "white",
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: "transparent",
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    padding: "10px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    color: "white",
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+    },
+  },
+}));
+
+export default SelectInput;

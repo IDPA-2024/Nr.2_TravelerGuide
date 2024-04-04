3d322dd2137878a9f016a6b07181cb09e9fddcae
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-04-04 15:46:42 +0200

    added api to verify

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -10,16 +10,34 @@ const page = () => {
   const router = useRouter();
 
   return (
-        <div className="bg-bg bg-cover h-screen w-screen flex justify-center items-center md:justify-end">
-          <div className="flex flex-col gap-20 md:gap-10 justify-center items-center w-full h-full bg-black/50 md:rounded-xl shadow-lg shadow-black backdrop-filter py-5 backdrop-blur-md md:h-2/3 md:w-1/3 md:mr-10">
-            <p className="font-bold text-white text-6xl">Verifizieren</p>
-            <p className="text-2xl text-center">
-              Es wurde eine Email gesendet. Bitte verifizieren Sie sich
-            </p>
-            <CustomButton text="Erneut senden" size="lg" onClick={() => {}} />
-            <CustomButton text="Anmelden" size="sm" onClick={() => {router.push("/login")}} />
-          </div>
-        </div>
+    <div className="bg-bg bg-cover h-screen w-screen flex justify-center items-center md:justify-end">
+      <div className="flex flex-col gap-20 md:gap-10 justify-center items-center w-full h-full bg-black/50 md:rounded-xl shadow-lg shadow-black backdrop-filter py-5 backdrop-blur-md md:h-2/3 md:w-1/3 md:mr-10">
+        <p className="font-bold text-white text-6xl">Verifizieren</p>
+        <p className="text-2xl text-center">
+          Es wurde eine Email gesendet. Bitte verifizieren Sie sich
+        </p>
+        <CustomButton
+          text="Erneut senden"
+          size="lg"
+          onClick={() => {
+            fetch("/api/auth/resendVerify", {
+              method: "POST",
+              body: JSON.stringify({ email: email }),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+          }}
+        />
+        <CustomButton
+          text="Anmelden"
+          size="sm"
+          onClick={() => {
+            router.push("/login");
+          }}
+        />
+      </div>
+    </div>
   );
 };
 

269250dc0946ad32bdc7459bf14c140a60e9b5c5
yannick <yannick.schlaepfer@lernende.bbw.ch>
2024-04-04 08:02:17 +0200

    Merge branch 'frontend' of https://github.com/IDPA-2024/Nr.2_TravelerGuide into frontend

--- src/app/components/tabs/Overview.tsx
+++ src/app/components/tabs/Overview.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { RiMapPinLine } from "react-icons/ri";
+import { IoMdTime } from "react-icons/io";
+import { GrMoney } from "react-icons/gr";
+import { IoRestaurantOutline } from "react-icons/io5";
+import { LuLeaf } from "react-icons/lu";
 
 const Overview = () => {
-    return (
-        <div>
-            <h1>RestaurantName</h1>
-        </div>
-    );
+    
+const openingHours = {
+    Monday: ["11:30-14:00", "17:00-22:00"],
+    Tuesday: ["11:30-14:00", "17:00-22:00"],
+    Wednesday: ["11:30-14:00", "17:00-22:00"],
+    Thursday: ["11:30-14:00", "17:00-22:00"],
+    Friday: ["11:30-14:00", "17:00-22:00"],
+  };
+const [localClosed, setLocalClosed] = React.useState(false);
+
+  return (
+    <div className="flex flex-col gap-10">
+      <h1 className="text-center font-bold text-2xl">RestaurantName</h1>
+      <div className="flex flex-col gap-6">
+        <p className="flex items-center gap-5">
+          <RiMapPinLine size={40} /> DieStrasse 15, 12345 Ortschaft
+        </p>
+        <p className="flex items-center gap-5">
+          <IoMdTime size={40} /> Heute Geschlossen oder 11:30 - 14:00 Uhr 17:00 - 22:00 Uhr
+        </p>
+
+        
+        <p className="flex items-center gap-5">
+          <GrMoney size={40} /> 4/5
+        </p>
+        <p className="flex items-center gap-5">
+          <IoRestaurantOutline size={40} /> Qualität
+        </p>
+        <p className="flex items-center gap-5">
+          <LuLeaf size={40} /> Vegan
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Overview;

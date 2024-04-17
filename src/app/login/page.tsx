565a1d36e7a13a73c9f08ed8f042bb5cb17f1881
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-04-17 13:43:44 +0200

    fixed fetch error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,7 +36,7 @@ const page = () => {
         theme: "dark",
       });
     } else {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
+      const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
